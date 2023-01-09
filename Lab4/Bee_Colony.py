fc011be1bcc5e1f6d1a9fbb1424064b7de9602dd
mshabanov27 <90135657+mshabanov27@users.noreply.github.com>
2023-01-09 20:06:58 +0400

    Update Bee_Colony.py

--- Lab4/Bee_Colony.py
+++ Lab4/Bee_Colony.py
@@ -4,52 +4,60 @@ import random
 
 class BeeColony:
     def __init__(self, graph_obj, graph_size):
-        self.graph = graph_obj
-        self.available_vertexes = [*range(graph_size)]
-        self.available_colors = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'black',
-                                 'white', 'grey', 'brown', 'cyan', 'magenta', 'coral', 'olive',
-                                 'teal', 'lavender', 'lime', 'beige', 'mint', 'maroon', 'silver', 'heather',
-                                 'iris', 'plum', 'jam', 'lilac', 'vine', 'grape', 'orchid', 'chiffon']
-        self.used_colors = []
+        self.__graph = graph_obj
+        self.__available_vertexes = [*range(graph_size)]
+        self.__available_colors = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'black',
+                                   'white', 'grey', 'brown', 'cyan', 'magenta', 'coral', 'olive',
+                                   'teal', 'lavender', 'lime', 'beige', 'mint', 'maroon', 'silver', 'heather',
+                                   'iris', 'plum', 'jam', 'lilac', 'vine', 'grape', 'orchid', 'chiffon']
+        self.__used_colors = []
 
     def scout_bee_work(self):
-        if len(self.available_vertexes) > 2:
-            best_nodes = random.sample(self.available_vertexes, 2)
-            if self.graph.adjacency_matrix[best_nodes[0]].count(1) > self.graph.adjacency_matrix[best_nodes[1]].count(1):
+        if len(self.__available_vertexes) > 2:
+            best_nodes = random.sample(self.__available_vertexes, 2)
+            if self.__graph.adjacency_matrix[best_nodes[0]].count(1) > self.__graph.adjacency_matrix[best_nodes[1]].count(1):
                 return best_nodes[0]
             else:
                 return best_nodes[1]
         else:
-            return self.available_vertexes[0]
+            return self.__available_vertexes[0]
 
     def onlooker_bee_work(self, best_node):
         self.__color_node(best_node)
         self.__color_neighbors(best_node)
-        self.available_vertexes.remove(best_node)
+        self.__available_vertexes.remove(best_node)
 
     def __color_node(self, best_node):
         color_iter = 0
-        self.graph.colors[best_node] = [self.available_colors[color_iter], 0]
-        while self.__check_same_color(self.available_colors[color_iter], best_node):
+        self.__graph.colors[best_node] = [self.__available_colors[color_iter], 0]
+        while self.__check_same_color(self.__available_colors[color_iter], best_node):
             color_iter += 1
-            self.graph.colors[best_node] = [self.available_colors[color_iter], 0]
-        if not self.available_colors[color_iter] in self.used_colors:
-            self.used_colors.append(self.available_colors[color_iter])
+            self.__graph.colors[best_node] = [self.__available_colors[color_iter], 0]
+        if not self.__available_colors[color_iter] in self.__used_colors:
+            self.__used_colors.append(self.__available_colors[color_iter])
 
     def __color_neighbors(self, best_node):
         color_iter = 0
-        for i in range(len(self.graph.adjacency_matrix[best_node])):
-            if self.graph.adjacency_matrix[best_node][i] == 1 and self.graph.colors[i][1] != 0:
-                self.graph.colors[i][0] = self.available_colors[color_iter]
-                while self.__check_same_color(self.graph.colors[i][0], i):
+        for i in range(len(self.__graph.adjacency_matrix[best_node])):
+            if self.__graph.adjacency_matrix[best_node][i] == 1 and self.__graph.colors[i][1] != 0:
+                self.__graph.colors[i][0] = self.__available_colors[color_iter]
+                while self.__check_same_color(self.__graph.colors[i][0], i):
                     color_iter += 1
-                    self.graph.colors[i][0] = self.available_colors[color_iter]
-                if not self.available_colors[color_iter] in self.used_colors:
-                    self.used_colors.append(self.available_colors[color_iter])
+                    self.__graph.colors[i][0] = self.__available_colors[color_iter]
+                if not self.__available_colors[color_iter] in self.__used_colors:
+                    self.__used_colors.append(self.__available_colors[color_iter])
             color_iter = 0
 
     def __check_same_color(self, color, node):
-        for i in range(len(self.graph.adjacency_matrix[node])):
-            if self.graph.adjacency_matrix[node][i] == 1 and self.graph.colors[i][0] == color:
+        for i in range(len(self.__graph.adjacency_matrix[node])):
+            if self.__graph.adjacency_matrix[node][i] == 1 and self.__graph.colors[i][0] == color:
                 return True
         return False
+
+    @property
+    def available_vertexes(self):
+        return self.__available_vertexes
+
+    @property
+    def used_colors(self):
+        return self.__used_colors
